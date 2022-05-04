72b00ad289b90e1a4acee8a5052469af918e7f71
tahmineh-tabarestani <80887743+tahmineh-tabarestani@users.noreply.github.com>
2022-05-04 18:27:59 +0000

    Add files via upload

---
+++ ros.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source /opt/ros/noetic/setup.bash
+source /root/rt2-ros1/devel/setup.bash
---
+++ ros2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source /opt/ros/foxy/setup.bash
+source /root/rt2-ros2/install/local_setup.bash
