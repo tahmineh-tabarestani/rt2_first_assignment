92279ae84a94196b03841b8773b054eca1079f44
tahmineh-tabarestani <80887743+tahmineh-tabarestani@users.noreply.github.com>
2022-05-04 18:14:39 +0000

    Add files via upload

---
+++ catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/root/ros2_main/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
