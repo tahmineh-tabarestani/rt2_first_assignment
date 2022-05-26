befca8b76782243ac60a014a22a29040dc159969
tahmineh-tabarestani <80887743+tahmineh-tabarestani@users.noreply.github.com>
2022-05-26 12:28:32 +0200

    del1

--- catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/root/ros2_action/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/root/ros2_main/devel;/opt/ros/noetic'.split(';') if '/root/ros2_main/devel;/opt/ros/noetic' != '' else []
